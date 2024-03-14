f8a91b5a6988c6c377f33ed641da39a8f0e637f2
BenAwuor2 <99531759+BenAwuor2@users.noreply.github.com>
2024-03-14 09:56:28 +0300

    test

---
+++ 0x00-ES6_basic/0-constants.test.js
@@ -0,0 +1,6 @@
+import {taskFirst, getLast, taskNext} from './0-constants';
+test('return a string', () => {
+    expect(taskFirst()).toBe("I prefer const when I can.");
+    expect( getLast()).toBe(" is okay");
+    expect(taskNext()).toBe("But sometimes let is okay");
+})
